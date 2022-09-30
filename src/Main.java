75ab35847fd5e4aa56062bd3dc5c3a7bf3110fe0
samuel powers <powerss7@mail.uc.edu>
2022-09-30 16:02:58 -0400

    it worked

--- src/Main.java
+++ src/Main.java
@@ -2,82 +2,143 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args)
-    //nested IF
     {
         Scanner in = new Scanner(System.in);
         String playerA = "";
         String playerB = "";
-        String trash = "";
+        String playAgain = "Y";
+        boolean playing = true;
+        boolean PlayAgainTest = true;
+        boolean AMoveTesting = true;
+        boolean BMoveTesting = true;
 
-        System.out.print("PlayerA pleas enter your move: ");
-        playerA = in.nextLine();
-
-        System.out.print("PlayerB pleas enter your move: ");
-        playerB = in.nextLine();
-
-
-        if(playerA.equalsIgnoreCase("R"))
+        do //the game
         {
-            // playerB code crossed agains thee playerA Rock move
-            if(playerB.equalsIgnoreCase("R"))
+            do // player A input loop
             {
-                System.out.println("Rock vs Rock. it is a TIE!");
-            }
-            else if (playerB.equalsIgnoreCase("P"))
+                System.out.print("PlayerA pleas enter your move: ");
+                playerA = in.nextLine();
+
+                if (playerA.equalsIgnoreCase("R"))
+                {
+                    do // player B input loop MK1
+                    {
+                        AMoveTesting = false;
+                        System.out.print("PlayerB pleas enter your move: ");
+                        playerB = in.nextLine();
+
+                        if (playerB.equalsIgnoreCase("R"))
+                        {
+                            BMoveTesting = false;
+                            System.out.println("Rock vs Rock. it is a TIE!");
+                        }
+                        else if (playerB.equalsIgnoreCase("P"))
+                        {
+                            BMoveTesting = false;
+                            System.out.println("Paper covers Rock. player B wins!");
+                        }
+                        else if (playerB.equalsIgnoreCase("S")) {
+                            BMoveTesting = false;
+                            System.out.println("Rock breaks scissors! player A wins!");
+                        }
+                        else
+                        {
+                            System.out.println(playerB + " is not a valid answer");
+                        }
+                    }
+                    while (BMoveTesting == true);
+                }
+                else if (playerA.equalsIgnoreCase("P"))
+                {
+                    do //player B input loop MK 2
+                    {
+
+                        AMoveTesting = false;
+                        System.out.print("PlayerB pleas enter your move: ");
+                        playerB = in.nextLine();
+
+                        if (playerB.equalsIgnoreCase("R"))
+                        {
+                            BMoveTesting = false;
+                            System.out.println("Paper covers Rock! player A wins!");
+                        }
+                        else if (playerB.equalsIgnoreCase("P"))
+                        {
+                            BMoveTesting = false;
+                            System.out.println("Paper vs Paper! it is a TIE!");
+                        }
+                        else if (playerB.equalsIgnoreCase("S"))
+                        {
+                            BMoveTesting = false;
+                            System.out.println("Scissors cuts paper! player B wins!");
+                        }
+                        else
+                        {
+                            System.out.println(playerB + " is not a valid answer");
+                        }
+                    } while (BMoveTesting == true);
+                }
+                else if (playerA.equalsIgnoreCase("S"))
+                {
+                    do // player B input loop MK 3
+                    {
+                        AMoveTesting = false;
+                        System.out.print("PlayerB pleas enter your move: ");
+                        playerB = in.nextLine();
+
+                        if (playerB.equalsIgnoreCase("R"))
+                        {
+                            BMoveTesting = false;
+                            System.out.println("Rock breaks Scissors! player B wins!");
+                        }
+                        else if (playerB.equalsIgnoreCase("P"))
+                        {
+                            BMoveTesting = false;
+                            System.out.println("Scissors cut Paper! player A wins!");
+                        }
+                        else if (playerB.equalsIgnoreCase("S"))
+                        {
+                            BMoveTesting = false;
+                            System.out.println("Scissors vs Scissors! it is a TIE!");
+                        }
+                        else
+                        {
+                            System.out.println(playerB + " is not a valid answer");
+                        }
+                    } while (BMoveTesting == true);
+                }
+                else
+                {
+                    System.out.println(playerA + " is not a valid answer");
+                }
+
+            } while (AMoveTesting == true);
+
+            do //do you want to play a game
             {
-                System.out.println("Paper covers Rock. player B wins!");
-            }
-            else if (playerB.equalsIgnoreCase("S"))
-            {
-                System.out.println("Rock breaks scissors! player A wins!");
-            }
-            else
-            {
-                trash = in.nextLine();
-                System.out.println(trash + " is not a valid number");
-            }
-        }
-        else if(playerA.equalsIgnoreCase("P"))
-        {
-            // playerB code crossed agains thee playerA paper move
-            if(playerB.equalsIgnoreCase("R"))
-            {
-                System.out.println("Paper covers Rock! player A wins!");
-            }
-            else if (playerB.equalsIgnoreCase("P"))
-            {
-                System.out.println("Paper vs Paper! it is a TIE!");
-            }
-            else if (playerB.equalsIgnoreCase("S"))
-            {
-                System.out.println("Rock breaks scissors! player A wins!");
-            }
-            else
-            {
-                trash = in.nextLine();
-                System.out.println(trash + " is not a valid number");
-            }
-        }
-        else
-        {
-            // playerB code crossed agains thee playerA scissor move
-            if(playerB.equalsIgnoreCase("R"))
-            {
-                System.out.println("Rock breaks Scissors! player B wins!");
-            }
-            else if (playerB.equalsIgnoreCase("P"))
-            {
-                System.out.println("Scissors cut Paper! player A wins!");
-            }
-            else if (playerB.equalsIgnoreCase("S"))
-            {
-                System.out.println("Rock breaks scissors! player A wins!");
-            }
-            else
-            {
-                trash = in.nextLine();
-                System.out.println(trash + " is not a valid number");
-            }
-        }
+                System.out.print("Would you like to play again? [Y/N] ");
+                playAgain = in.nextLine();
+                if (playAgain.equalsIgnoreCase("Y"))
+                {
+                    //System.out.println("play again");
+                    playing = true;
+                    PlayAgainTest = false;
+                    AMoveTesting = true;
+                    BMoveTesting = true;
+                }
+                else if (playAgain.equalsIgnoreCase("N"))
+                {
+                    System.out.println("Thanks for playing");
+                    playing = false;
+                    PlayAgainTest = false;
+                    System.exit(0);
+                }
+                else
+                {
+                    System.out.println(playAgain + " is not a valid answer");
+                }
+            } while (PlayAgainTest == true);
+            PlayAgainTest = true;// this is set after the loop to set up for the replay
+        } while (playing == true);
     }
 }
